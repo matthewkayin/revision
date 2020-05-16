ed056912516b1a590ebf1531cff60d2f0d0a6641
matthewkayin <matthewkayin@gmail.com>
2020-05-15 19:00:23 -0500

    Beginnings of a post reader page
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

--- home.js
+++ home.js
@@ -1,24 +1,48 @@
 var scroll_pos = window.scrollY;
 window.onscroll = function(){
 
-    var current_scroll_pos = window.scrollY;
-    if(current_scroll_pos == 0){
+    if(focus_scroll_point == -1){
 
-        document.getElementById("header").style.top = "0px";
+        var current_scroll_pos = window.scrollY;
+        if(current_scroll_pos == 0){
+
+            document.getElementById("header").style.top = "0px";
+
+        }else{
+
+            var current_top = document.getElementById("header").getBoundingClientRect().y;
+            var scroll_offset = current_scroll_pos - scroll_pos;
+            if(scroll_offset > 0 && current_top > -52){
+
+                document.getElementById("header").style.top = String(Math.max(-52, current_top - scroll_offset)) + "px";
+
+            }else if(scroll_offset < 0 && current_top < 0){
+
+                document.getElementById("header").style.top = String(Math.min(0, current_top - scroll_offset)) + "px";
+            }
+            scroll_pos = current_scroll_pos;
+        }
 
     }else{
 
-        var current_top = document.getElementById("header").getBoundingClientRect().y;
-        var scroll_offset = current_scroll_pos - scroll_pos;
-        if(scroll_offset > 0 && current_top > -52){
+        var current_scroll_pos = window.scrollY;
+        if((current_scroll_pos > focus_scroll_point && focus_scroll_direction == 1) || (current_scroll_pos < focus_scroll_point && focus_scroll_direction == -1)){
 
-            document.getElementById("header").style.top = String(Math.max(-52, current_top - scroll_offset)) + "px";
-
-        }else if(scroll_offset < 0 && current_top < 0){
-
-            document.getElementById("header").style.top = String(Math.min(0, current_top - scroll_offset)) + "px";
+            window.scrollTo(0, focus_scroll_point);
+            current_scroll_pos = window.scrollY;
         }
-        scroll_pos = current_scroll_pos;
+        if(current_scroll_pos == focus_scroll_point){
+
+            focus_reader();
+
+        }else{
+
+            var scroll_offset = current_scroll_pos - scroll_pos;
+            var current_header_top = document.getElementById("header").getBoundingClientRect().y;
+            var current_navbar_top = document.getElementById("navbar").getBoundingClientRect().bottom;
+            document.getElementById("header").style.top = String(Math.max(-52, current_header_top - Math.abs(scroll_offset))) + "px";
+            document.getElementById("navbar").style.bottom = String(Math.max(-60, current_header_top - Math.abs(scroll_offset))) + "px";
+        }
     }
 };
 
@@ -79,4 +103,54 @@ function update_theme(){
 
         usertheme: current_theme
     }));
+}
+
+var focus_scroll_point = -1;
+var focus_scroll_direction = 0;
+
+function toggle_reader_focus(){
+
+    if(focus_scroll_point != -1){
+
+        return;
+    }
+
+    var currently_focused = document.getElementById("page-reader").style.overflow == "scroll";
+
+    if(currently_focused){
+
+        document.getElementById("navbar").style.bottom = "0px";
+        document.getElementById("page-reader").style.overflow = "hidden";
+        document.body.style.overflow = "initial";
+
+    }else{
+
+        focus_scroll_point = document.getElementById("page-reader").offsetTop;
+        if(focus_scroll_point == window.scrollY){
+
+            focus_reader();
+
+        }else{
+
+            if(focus_scroll_point > window.scrollY){
+
+                focus_scroll_direction = 1;
+
+            }else{
+
+                focus_scroll_direction = -1;
+            }
+            window.scrollTo({top: focus_scroll_point, behavior: "smooth"});
+        }
+    }
+}
+
+function focus_reader(){
+
+    document.getElementById("header").style.top = "-52px";
+    document.getElementById("navbar").style.bottom = "-60px";
+    document.body.style.overflow = "hidden";
+    document.getElementById("page-reader").style.overflow = "scroll";
+    focus_scroll_point = -1;
+    focus_scroll_direction = 0;
 }
